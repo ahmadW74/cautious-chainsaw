8b49082ff214fb63c793c95804c0fb8c3abfe674
Ahmad Waqar <ahmadwaq2008@gmail.com>
2025-07-09 14:42:19 +0500

    iop

--- src/SampleGraph.jsx
+++ src/SampleGraph.jsx
@@ -201,7 +201,7 @@ const SampleGraph = ({
     <div className="relative">
       <Card className="w-full bg-card border-border">
         <CardContent className="relative px-6 py-6 lg:px-8 lg:py-8 flex justify-center overflow-auto">
-          <div className="w-full overflow-hidden flex flex-col gap-4">
+          <div className="w-full overflow-visible flex flex-col gap-4">
             {summary && (
               <div className="text-left">
                 <h2 className="font-semibold text-lg text-foreground">
@@ -215,7 +215,7 @@ const SampleGraph = ({
               </div>
             )}
             <div
-              className=" overflow-hidden flex justify-center"
+              className=" overflow-visible flex justify-center"
               style={{
                 transform: `scale(2)`,
               }}
