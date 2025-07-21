e7253e8529b6730808bfe824dd3144f5985ed344
Ahmad Waqar <77718267+ahmadW74@users.noreply.github.com>
2025-07-21 16:12:24 +0500

    feat: add draggable subflow nodes with tooltips

---
+++ src/components/nodes/RecordNode.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Handle, Position } from '@xyflow/react';
+import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/ui/tooltip';
+
+export default function RecordNode({ data }) {
+  return (
+    <div className="relative flex flex-col items-center">
+      <Handle type="target" position={Position.Top} />
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div className="px-2 py-1 rounded border bg-background text-foreground text-xs">
+            {data.label}
+          </div>
+        </TooltipTrigger>
+        {data.tooltip && (
+          <TooltipContent className="whitespace-pre">
+            {data.tooltip}
+          </TooltipContent>
+        )}
+      </Tooltip>
+      <Handle type="source" position={Position.Bottom} />
+    </div>
+  );
+}
