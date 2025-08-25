1ba262e262c6b34ef120f6b2c21347c9e13291bc
Ahmad Waqar <ahmadwaq2008@gmail.com>
2025-08-25 19:02:24 +0500

    asA

--- src/pages/Goals2.jsx
+++ src/pages/Goals2.jsx
@@ -1,7 +1,8 @@
 import GlobeScene from "@/components/GlobeScene";
 
 // Update this path to point to your local globe model
-const globeModel = "/models/globe.obj";
+const globeModel =
+  "C:\\Users\\ahmad\\Downloads\\uploads_files_2501044_Low+Poly+Earth.obj";
 
 export default function Goals2() {
   return (
@@ -13,4 +14,3 @@ export default function Goals2() {
     </section>
   );
 }
-
